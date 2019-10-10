0e8c4f1347be31ee2009d839ded5d13da631ab3c
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2019-10-10 22:11:51 +0300

    upd to core 3

--- nugetall.ps1
+++ nugetall.ps1
@@ -4,5 +4,5 @@ cd $SolutionFolderPath
 $sign = Read-Host 'Enter sign'
 
 cd .\AspNetCore.Http\bin\Release
-nuget push DashboardCode.AspNetCore.Http.1.0.0.nupkg $sign -Source https://api.nuget.org/v3/index.json
+nuget push DashboardCode.AspNetCore.Http.3.0.0.nupkg $sign -Source https://api.nuget.org/v3/index.json
 
