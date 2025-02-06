02e40987e99af6572d54329125382db7226aef95
Roman Pokrovskij <roman.pokrovskij@gmail.com>
2025-02-06 12:21:57 +0200

    .core version shift

--- nugetall.ps1
+++ nugetall.ps1
@@ -4,5 +4,5 @@ cd $SolutionFolderPath
 $sign = Read-Host 'Enter sign'
 
 cd .\AspNetCore.Http\bin\Release
-nuget push DashboardCode.AspNetCore.3.0.6.nupkg $sign -Source https://api.nuget.org/v3/index.json
+nuget push DashboardCode.AspNetCore.4.0.0.nupkg $sign -Source https://api.nuget.org/v3/index.json
 
